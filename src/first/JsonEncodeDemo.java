52a9310c28872f8e4bc6b3a1fdcbd9d2a190b987
TA15 <TA15@172.30.12.28>
2017-10-11 14:03:49 +0300

    JSON

---
+++ src/first/JsonEncodeDemo.java
@@ -0,0 +1,18 @@
+package first;
+
+import org.json.simple.JSONObject;
+
+
+public class JsonEncodeDemo {
+	public static void main(String[] args){
+	      JSONObject obj = new JSONObject();
+
+	      obj.put("name", "foo");
+	      obj.put("num", new Integer(100));
+	      obj.put("balance", new Double(1000.21));
+	      obj.put("is_vip", new Boolean(true));
+
+	      System.out.print(obj.get("balance"));
+	   }
+
+}
